9412186d22506cc7b35fabf1d0735a4bf7d6b524
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-10-27 17:38:24 +0300

    Update models

--- project_automation_admin/admin_panel/models.py
+++ project_automation_admin/admin_panel/models.py
@@ -13,15 +13,18 @@ class ProjectManager(models.Model):
 
 class Student(models.Model):
     full_name = models.CharField(max_length=100, unique=True)
-    telegram_chat_id = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True, blank=True)
+    telegram_chat_id = models.CharField(max_length=50, unique=True, blank=True)
     level = models.CharField(max_length=50)
     registration_time = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50)
     team = models.ForeignKey(
         'Team',
         on_delete=models.CASCADE,
-        related_name='teams'
-        )
+        related_name='teams',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return f'{self.full_name} {self.level}'
